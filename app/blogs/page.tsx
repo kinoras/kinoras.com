c58e543ae0fc44fe4e7e135f701a953f2f151105
Hon Chi Kit <39506429+kinoras@users.noreply.github.com>
2025-09-30 00:53:40 +0800

    feat(ui/blogs): add banner section

---
+++ app/blogs/page.tsx
@@ -0,0 +1,32 @@
+import { IconMessage } from '@tabler/icons-react'
+
+import {
+    Banner,
+    BannerContent,
+    BannerDescription,
+    BannerIconBackground,
+    BannerTitle
+} from '@/components/ui/banner'
+
+const BlogsPage = () => {
+    return (
+        <main>
+            <Banner className="bg-theme/15">
+                <BannerContent>
+                    <BannerTitle className="mb-1 -ml-[2px] text-5xl sm:mb-0 md:-ml-[3px]">
+                        Blogs
+                    </BannerTitle>
+                    <BannerDescription className="text-lg text-balance sm:max-w-2/3">
+                        I document the development process of my projects here.
+                    </BannerDescription>
+                </BannerContent>
+                <BannerIconBackground
+                    className="hidden sm:block"
+                    icon={IconMessage}
+                />
+            </Banner>
+        </main>
+    )
+}
+
+export default BlogsPage

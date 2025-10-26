9837bc74719ed8f2dc5fa88a8d77fc9122c01bb6
Hon Chi Kit <39506429+kinoras@users.noreply.github.com>
2025-10-27 07:52:45 +0800

    fix(data/blogs): shorten cache ttl to avoid img expiry

--- integrations/notion/post/index.ts
+++ integrations/notion/post/index.ts
@@ -15,18 +15,18 @@ export const Post: PostService = {
         unstable_cache(
             async () => getPostList(options), // Fetcher
             ['posts', JSON.stringify(options || {})], // Key
-            { revalidate: 3600, tags: ['posts'] } // TTL & tags
+            { revalidate: 600, tags: ['posts'] } // TTL (10 min) & tags
         )(),
     getSingle: (id) =>
         unstable_cache(
             async () => getSinglePost(id), // Fetcher
             ['post', `${id}`], // Key
-            { revalidate: 3600, tags: ['post', `post-${id}`] } // TTL & tags
+            { revalidate: 1800, tags: ['post', `post-${id}`] } // TTL (30 min) & tags
         )(),
     getContent: (id) =>
         unstable_cache(
             async () => getPostContent(id), // Fetcher
             ['post-content', `${id}`], // Key
-            { revalidate: 3600, tags: ['post-content', `post-${id}`] } // TTL & tags
+            { revalidate: 1800, tags: ['post-content', `post-${id}`] } // TTL (30 min) & tags
         )()
 }

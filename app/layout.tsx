adaffeadc9780f962579281df946d6bc2f41cb55
Hon Chi Kit <39506429+kinoras@users.noreply.github.com>
2025-11-08 13:15:53 +0800

    feat(ui/footer): create footer layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,32 +4,21 @@ import { Geist, Geist_Mono, Noto_Sans_JP } from 'next/font/google'
 
 import LoadingProgress from 'nextjs-toploader'
 
+import Footer from '@/components/layout/footer'
 import Header from '@/components/layout/header'
 
 import { cn } from '@/lib/utils'
 
 import './globals.css'
 
-const latinSans = Geist({
-    variable: '--font-latin-sans',
-    subsets: ['latin']
-})
+// Fonts
+const latinSans = Geist({ variable: '--font-latin-sans', subsets: ['latin'] })
+const latinMono = Geist_Mono({ variable: '--font-latin-mono', subsets: ['latin'] })
+const cjkSans = Noto_Sans_JP({ variable: '--font-cjk-sans', subsets: ['latin'] })
 
-const latinMono = Geist_Mono({
-    variable: '--font-latin-mono',
-    subsets: ['latin']
-})
-
-const cjkSans = Noto_Sans_JP({
-    variable: '--font-cjk-sans',
-    subsets: ['latin']
-})
-
+// Metadata
 export const metadata: Metadata = {
-    title: {
-        template: '%s | kinoRAS',
-        default: 'kinoRAS'
-    },
+    title: { template: '%s | kinoRAS', default: 'kinoRAS' },
     description: 'I am a web developer from Macau.'
 }
 
@@ -39,13 +28,14 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="zh-tw" suppressHydrationWarning>
+        <html suppressHydrationWarning>
             <body
                 className={cn(
                     latinSans.variable,
                     latinMono.variable,
                     cjkSans.variable,
                     'bg-background text-primary font-sans',
+                    'flex min-h-svh flex-col [&>main]:flex-1',
                     'antialiased'
                 )}
             >
@@ -64,6 +54,7 @@ export default function RootLayout({
                     />
                     <Header />
                     {children}
+                    <Footer />
                 </ThemeProvider>
             </body>
         </html>
---
+++ components/layout/footer/index.tsx
@@ -0,0 +1,9 @@
+const PageFooter = () => {
+    return (
+        <footer className="wrapper">
+            <div className="container">{/* Footer content */}</div>
+        </footer>
+    )
+}
+
+export default PageFooter

85795dd046d55b3c1456e69037db5de143a4dba7
Hon Chi Kit <39506429+kinoras@users.noreply.github.com>
2025-11-09 16:13:32 +0800

    chore(ui/home): organize banner structure

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,32 @@
+import RotatingWords from '@/components/custom/rotating-words'
+import { Banner, BannerBar, BannerContent, BannerTitle } from '@/components/ui/banner'
 import { PassageBody } from '@/components/ui/passage'
 import { Section, SectionTitle } from '@/components/ui/section'
 
 import { profile } from '@/data/about-profile'
+import { rotatingWords } from '@/data/home-banner'
 
-import Banner from './banner'
 import { HighlightedStrong, HomeParagraph } from './components'
 
 export default function Home() {
     return (
         <main>
-            <Banner />
+            <Banner className="transition-all">
+                <BannerContent>
+                    <BannerTitle className="text-[min(11vw,48px)] whitespace-nowrap transition-all duration-400">
+                        Hello,
+                        <br />
+                        I&nbsp;
+                        <RotatingWords className="w-full" delay={150}>
+                            {...rotatingWords}
+                            <span>
+                                am <b className="text-theme font-medium">kinoRAS</b>.
+                            </span>
+                        </RotatingWords>
+                    </BannerTitle>
+                    <BannerBar />
+                </BannerContent>
+            </Banner>
 
             <Section>
                 <SectionTitle>About me</SectionTitle>
---
+++ data/home-banner.tsx
@@ -0,0 +1,8 @@
+import type { ReactNode } from 'react'
+
+export const rotatingWords: ReactNode[] = [
+    'am from Macau.',
+    'listen to music.',
+    'studied in NTU.',
+    'develop websites.'
+]

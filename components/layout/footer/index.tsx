cc88ecbe5fd1dd191f9f9f44b972901a442c3b4e
Hon Chi Kit <39506429+kinoras@users.noreply.github.com>
2025-11-09 20:09:50 +0800

    feat(footer): add footer content

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -1,7 +1,36 @@
-const PageFooter = () => {
+import type { ComponentProps } from 'react'
+
+import { Anchor } from '@/components/ui/typography'
+
+import { cn } from '@/lib/utils'
+
+import { links } from '@/data/footer-links'
+
+const PageFooter = ({ className, ...props }: ComponentProps<'footer'>) => {
     return (
-        <footer className="wrapper">
-            <div className="container">{/* Footer content */}</div>
+        <footer className={cn('wrapper border-border mt-6 border-t', className)} {...props}>
+            <div className="container">
+                <div className="flex items-center justify-between py-6">
+                    <span className="text-secondary">© 2025 kinoRAS</span>
+                    <ul className="flex items-center gap-5 sm:gap-5">
+                        {links.map(({ icon: LinkIcon, href, title }) => (
+                            <li key={href}>
+                                <Anchor
+                                    href={href}
+                                    styled={false}
+                                    className="[&>svg]:opacity-55 hover:[&>svg]:opacity-100"
+                                >
+                                    <LinkIcon
+                                        className="size-5 transition-opacity"
+                                        strokeWidth={1.5}
+                                    />
+                                    <span className="sr-only">{title}</span>
+                                </Anchor>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </footer>
     )
 }

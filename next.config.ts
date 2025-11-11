f7910502bfdbff3b31c34fa07e334442b665d59f
Hon Chi Kit <39506429+kinoras@users.noreply.github.com>
2025-11-11 12:35:37 +0800

    chore(ui/about): use pre-optimized webp icons

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     /* config options here */
-    cacheComponents: true
+    cacheComponents: true,
+    images: { unoptimized: true }
 }
 
 export default nextConfig
